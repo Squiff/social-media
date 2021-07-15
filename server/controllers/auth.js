359501caeba2099b1288734c950311464ba36d5e
Paul Asquith <p.n.asquith@gmail.com>
2021-07-15 23:31:22 +0100

    add schema and firebase models

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,6 @@
-const { firebase, admin } = require('../firebase');
+const { firebase } = require('../firebase');
 const Joi = require('joi');
-const { isSchema } = require('joi');
-
-const db = admin.firestore();
+const User = require('../models/User');
 
 async function signUp(req, res) {
     const { email, password, username } = req.body;
@@ -14,30 +12,29 @@ async function signUp(req, res) {
         return res.status(400).json({ success: false, message: error.message });
     }
 
-    const unameQuery = await db.collection('users').where('username', '==', username).get();
+    const usernameExists = await User.getByUserName(username);
 
-    if (!unameQuery.empty)
+    if (usernameExists)
         return res.status(400).json({ success: false, message: 'username already exists' });
 
+    // Create Auth and User Record
     try {
-        // Create Auth and User Record
-        const token = await createAuthUser({ email, password });
+        const authRecord = await firebase.auth().createUserWithEmailAndPassword(email, password);
+        const token = await authRecord.user.getIdToken();
 
-        const newUserRecord = {
+        const user = new User({
+            uid: authRecord.user.uid,
             email,
             username,
-            dateadded: admin.firestore.Timestamp.now(),
-        };
+        });
 
-        const user = await db.collection('users').add(newUserRecord);
+        await user.add();
 
-        res.status(201).json({ success: true, message: `user signed up`, id: user.id, token });
+        res.status(201).json({ success: true, message: `user signed up`, token });
     } catch (error) {
         let statusCode = 500;
 
         if (error.code === 'auth/email-already-in-use') statusCode = 400;
-
-        console.log(error);
 
         return res
             .status(statusCode)
@@ -53,25 +50,25 @@ const signUpSchema = Joi.object({
     username: Joi.string().required().min(1).trim(),
 });
 
-/** Create firebase authentication record for user and return an ID token (JWT) */
-async function createAuthUser({ email, password }) {
-    const userRecord = await firebase.auth().createUserWithEmailAndPassword(email, password);
-    const token = await userRecord.user.getIdToken();
-    return token;
-}
-
 async function login(req, res) {
     const { email, password } = req.body;
 
+    // VALIDATION
+    const { error, value } = loginSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ success: false, message: error.message });
+    }
+
     try {
-        const userRecord = await firebase.auth().signInWithEmailAndPassword(email, password);
-        const token = await userRecord.user.getIdToken();
+        const authRecord = await firebase.auth().signInWithEmailAndPassword(email, password);
+        const token = await authRecord.user.getIdToken();
         res.status(201).json({ success: true, message: `user logged in`, token });
     } catch (error) {
         let statusCode = 500;
 
-        if (error.code === 'auth/wrong-password') statusCode = 403;
-        if (error.code === 'auth/user-not-found') statusCode = 403;
+        if (error.code === 'auth/wrong-password') statusCode = 401;
+        if (error.code === 'auth/user-not-found') statusCode = 401;
 
         return res
             .status(statusCode)
@@ -81,9 +78,7 @@ async function login(req, res) {
 
 const loginSchema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string()
-        .regex(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}/)
-        .required(),
+    password: Joi.string().required(),
 });
 
 module.exports = { signUp, login };
